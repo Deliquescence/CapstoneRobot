3861aec82a552ae6becf8353251f60faeb207cdd
Evan Haskell <ehaskell@oakland.edu>
2019-01-21 22:16:16 -0500

    Refactor picar_driver and picar_server
    This mainly extracts the global variables shared between the two
    modules into an object that is passed to the grpc server when it
    is created. The files have also been reformatted.

--- src/RobotServer/picar_driver.py
+++ src/RobotServer/picar_driver.py
@@ -1,217 +1,185 @@
-from picar import front_wheels
 import picar
 import cv2
-from concurrent import futures
 import time
 import picar_server
 import picar_helper
 import socket
-import grpc
-import picar_pb2
-import picar_pb2_grpc
 import numpy as np
 
 picar.setup()
-rear_wheels_enabled = True
-front_wheels_enabled = True
 
-fw = front_wheels.Front_Wheels()
-fw_default = picar_helper.getDefaultAngle(socket.gethostname())
-
-FW_ANGLE_MAX = fw_default+30
-FW_ANGLE_MIN = fw_default-30
-
-mode = 0
-
-baseTopLeft = None
-baseTopRight = None
-baseBottomRight = None
-baseBottomLeft = None
-
-baseTopEdge = None
-baseRightEdge = None
-baseBottomEdge = None
-baseLeftEdge = None
-baseAvgEdge = None
-
-baseMidX = None
-baseMidY = None
-baseMidPoint = None
-
-maxTagDisplacement = None
-hasBaseCorners = False
-
-#det up our tag dictionary and parameter value 
-#we use tag ids 1,2,4,8
+# det up our tag dictionary and parameter value
+# we use tag ids 1,2,4,8
 arDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
 parameters = cv2.aruco.DetectorParameters_create()
 
-
-#get a reference to the camera, default is 0
+# get a reference to the camera, default is 0
 camera = cv2.VideoCapture(0)
-frame = None
+
 
 def main():
-    global baseTopLeft, baseTopRight, baseBottomRight, baseBottomLeft
-    global baseTopEdge, baseRightEdge, baseBottomEdge, baseLeftEdge, baseAvgEdge
-    global baseMidX, baseMidY, baseMidPoint
-    global maxTagDisplacement, frame, hasBaseCorners
+    driver = PiCarDriver()
 
-    #start the server
-    server = picar_server.getServer()
+    # start the server
+    server = picar_server.getServer(driver)
     server.start()
 
-    print "Server Started on "+socket.gethostname()+"\n"
-    print "Press Ctrl-C to quit"
+    print "Server Started on " + socket.gethostname() + "\n"
+    print "Press q to quit"
 
-    picar_helper.move(0.0,0.0)
+    # start the driver
+    driver.run()
 
-    # loop unless break occurs
-    while True:
-
-        #check if key pressed
-        k = cv2.waitKey(1) & 0xFF
-
-        #if q key is pressed we break loop
-        if k == ord('q'):
-            break
-
-        # get reference to current mode
-        mode = picar_server.mode
-        #get the current frame
-        (grabbed, frame) = camera.read()
-
-        if mode == 1:
-            # leader mode
-            #print "picar set to LEADER"
-            picar_helper.move((picar_server.throttle/2), picar_server.direction)
-        elif mode == 2:
-            # follower mode
-            #if no base corners, get corners
-            if not hasBaseCorners:
-                getBaseCorners()
-            #if we have base corners, now enter follower
-            if hasBaseCorners:
-                throttle, direction = tagID()
-                picar_helper.move(throttle, direction)
-            else:
-                print "Base Tag Corners Not Detected!"
-        else:
-            picar_helper.move(0.0, 0.0)
-            baseTopLeft = None
-            baseTopRight = None
-            baseBottomRight = None
-            baseBottomLeft = None
-
-            baseTopEdge = None
-            baseRightEdge = None
-            baseBottomEdge = None
-            baseLeftEdge = None
-            baseAvgEdge = None
-
-            baseMidX = None
-            baseMidY = None
-            baseMidPoint = None
-
-            maxTagDisplacement = None
-            hasBaseCorners = False
-
-
-        #set frame to send to desktop
-        picar_server.setFrame(frame)
-        time.sleep(1/30)
-
-    #cleanup
+    # cleanup
     destroy()
 
 
-#method to recognize tags
-def tagID():
-    #setting up our frame
-    global frame
+class PiCarDriver(object):
+    def __init__(self):
+        self.mode = 0
+        self.frame = cv2.imread('init.jpg')
+        self.next_throttle_and_dir = (0.0, 0.0)
+
+    def set_throttle_and_dir(self, throttle, direction):
+        self.next_throttle_and_dir = (throttle, direction)
+
+    def run(self):
+        picar_helper.move(0.0, 0.0)
+
+        # loop unless break occurs
+        while True:
+            # check if key pressed
+            k = cv2.waitKey(1) & 0xFF
+
+            # if q key is pressed we break loop
+            if k == ord('q'):
+                break
+
+            # get the current frame
+            _, self.frame = camera.read()
+
+            if self.mode == 1:
+                # leader mode
+                picar_helper.move(self.next_throttle_and_dir[0] / 2,
+                                  self.next_throttle_and_dir[1])
+            elif self.mode == 2:
+                # follower mode
+                # if no base corners, get corners
+                bc = getBaseCorners(self.frame)
+                if bc is not None:
+                    throttle, direction = tagID(self.frame, bc)
+                    picar_helper.move(throttle, direction)
+                else:
+                    print "Base Tag Corners Not Detected!"
+            else:
+                picar_helper.move(0.0, 0.0)
+
+            time.sleep(1 / 30)
+
+
+class BaseCorners(object):
+    pass
+
+
+# method to recognize tags
+def tagID(frame, bc):
+    # setting up our frame
     img = np.array(frame, dtype=np.uint8)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    #gather the parameters of the markers  ID is important to us    
-    corners, ids, reject = cv2.aruco.detectMarkers(gray, arDict, parameters=parameters)
-    
+
+    # gather the parameters of the markers  ID is important to us
+    corners, ids, reject = cv2.aruco.detectMarkers(gray, arDict,
+                                                   parameters=parameters)
 
     if ids is not None:
-        #CORNER LAYOUT: corner[0][corner][x][y]
-        #starts top left and moves clockwise
+        # CORNER LAYOUT: corner[0][corner][x][y]
+        # starts top left and moves clockwise
         tLeft = corners[0][0][0]
         tRight = corners[0][0][1]
         bRight = corners[0][0][2]
         bLeft = corners[0][0][3]
 
-        #draw rectangle around detected tag and baseline 
-        cv2.rectangle(frame, (baseTopLeft[0],baseTopLeft[1]),(baseBottomRight[0],baseBottomRight[1]), (0,255,0), 2)
-        cv2.rectangle(frame, (tLeft[0],tLeft[1]),(bRight[0], bRight[1]), (0,0,255), 2)
-        
-        #insert rest of follower code here
-        topEdge = (tRight[0]-tLeft[0], tRight[1]-tLeft[1])
-        rightEdge = (bRight[0]-tRight[0], bRight[1]-tRight[1])
-        bottomEdge = (bRight[0]-bLeft[0], bRight[1]-bLeft[1])
-        leftEdge = (bLeft[0]- tLeft[0], bLeft[1]-tLeft[1])
-        avgEdge = (topEdge[0] + rightEdge[1] + bottomEdge[0] + leftEdge[1])/4
-        
-        # calculate speed from avg edge comparison
-        speedVar = 1.0 - (avgEdge/baseAvgEdge)
+        # draw rectangle around detected tag and baseline
+        cv2.rectangle(frame, (bc.baseTopLeft[0], bc.baseTopLeft[1]),
+                      (bc.baseBottomRight[0], bc.baseBottomRight[1]),
+                      (0, 255, 0),
+                      2)
+        cv2.rectangle(frame, (tLeft[0], tLeft[1]), (bRight[0], bRight[1]),
+                      (0, 0, 255), 2)
 
-        tagMidX = (topEdge[0]/2)+bLeft[0]
-        tagMidY = (rightEdge[1]/2)+tRight[1]
+        # insert rest of follower code here
+        topEdge = (tRight[0] - tLeft[0], tRight[1] - tLeft[1])
+        rightEdge = (bRight[0] - tRight[0], bRight[1] - tRight[1])
+        bottomEdge = (bRight[0] - bLeft[0], bRight[1] - bLeft[1])
+        leftEdge = (bLeft[0] - tLeft[0], bLeft[1] - tLeft[1])
+        avgEdge = (topEdge[0] + rightEdge[1] + bottomEdge[0] + leftEdge[1]) / 4
+
+        # calculate speed from avg edge comparison
+        speedVar = 1.0 - (avgEdge / bc.baseAvgEdge)
+
+        tagMidX = (topEdge[0] / 2) + bLeft[0]
+        tagMidY = (rightEdge[1] / 2) + tRight[1]
         tagMidPoint = (tagMidX, tagMidY)
-        
-        # calculates what fraction of total displacement occurs in X direction, should be number between -1.0 and 1.0
-        tagXDisplacement = tagMidPoint[0] - baseMidPoint[0]
-        tagDisplacementAmt = tagXDisplacement/maxTagDisplacement
+
+        # calculates what fraction of total displacement occurs in X direction,
+        # should be number between -1.0 and 1.0
+        tagXDisplacement = tagMidPoint[0] - bc.baseMidPoint[0]
+        tagDisplacementAmt = tagXDisplacement / bc.maxTagDisplacement
         tagThreshold = 10
 
-        if (avgEdge < baseAvgEdge-tagThreshold):
-            #too far from leader, move closer
-            return(speedVar, tagDisplacementAmt)
-        elif (avgEdge > baseAvgEdge+tagThreshold):
-            #too close to leader, move away
-            return(-0.3, -tagDisplacementAmt)
+        if avgEdge < bc.baseAvgEdge - tagThreshold:
+            # too far from leader, move closer
+            return speedVar, tagDisplacementAmt
+        elif avgEdge > bc.baseAvgEdge + tagThreshold:
+            # too close to leader, move away
+            return -0.3, -tagDisplacementAmt
         else:
-            return (0.0, 0.0)
+            return 0.0, 0.0
     else:
-        return(0.0, 0.0)
+        return 0.0, 0.0
 
-def getBaseCorners():
-    global baseTopLeft, baseTopRight, baseBottomRight, baseBottomLeft
-    global baseTopEdge, baseRightEdge, baseBottomEdge, baseLeftEdge, baseAvgEdge
-    global baseMidX, baseMidY, baseMidPoint
-    global maxTagDisplacement, frame, hasBaseCorners
 
+def getBaseCorners(frame):
     grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    corners, ids, reject = cv2.aruco.detectMarkers(grayImg, arDict, parameters=parameters)
+    corners, ids, reject = cv2.aruco.detectMarkers(grayImg, arDict,
+                                                   parameters=parameters)
     if ids is not None:
         if corners is not None:
-            baseTopLeft = corners[0][0][0]
-            baseTopRight = corners[0][0][1]
-            baseBottomRight = corners[0][0][2]
-            baseBottomLeft = corners[0][0][3]
+            bc = BaseCorners()
+            bc.baseTopLeft = corners[0][0][0]
+            bc.baseTopRight = corners[0][0][1]
+            bc.baseBottomRight = corners[0][0][2]
+            bc.baseBottomLeft = corners[0][0][3]
 
-            baseTopEdge = (baseTopRight[0]-baseTopLeft[0], baseTopRight[1]-baseTopLeft[1])
-            baseRightEdge = (baseBottomRight[0]-baseTopRight[0], baseBottomRight[1]-baseTopRight[1])
-            baseBottomEdge = (baseBottomRight[0]-baseBottomLeft[0], baseBottomRight[1]-baseBottomLeft[1])
-            baseLeftEdge = (baseBottomLeft[0]-baseTopLeft[0], baseBottomLeft[1]-baseTopLeft[1])
-            baseAvgEdge = (baseTopEdge[0]+baseRightEdge[1]+baseBottomEdge[0]+baseLeftEdge[1])/4
+            bc.baseTopEdge = (
+                bc.baseTopRight[0] - bc.baseTopLeft[0],
+                bc.baseTopRight[1] - bc.baseTopLeft[1])
+            bc.baseRightEdge = (bc.baseBottomRight[0] - bc.baseTopRight[0],
+                                bc.baseBottomRight[1] - bc.baseTopRight[1])
+            bc.baseBottomEdge = (bc.baseBottomRight[0] - bc.baseBottomLeft[0],
+                                 bc.baseBottomRight[1] - bc.baseBottomLeft[1])
+            bc.baseLeftEdge = (
+                bc.baseBottomLeft[0] - bc.baseTopLeft[0],
+                bc.baseBottomLeft[1] - bc.baseTopLeft[1])
+            bc.baseAvgEdge = (bc.baseTopEdge[0] + bc.baseRightEdge[1] +
+                              bc.baseBottomEdge[0] +
+                              bc.baseLeftEdge[1]) / 4
 
-            baseMidX = (baseTopEdge[0]/2)+baseTopLeft[0]
-            baseMidY = (baseRightEdge[1]/2)+baseTopRight[1]
-            baseMidPoint = (baseMidX, baseMidY)
-            maxTagDisplacement = baseMidPoint[0]-(baseTopEdge[0]/2)
-            hasBaseCorners = True
+            bc.baseMidX = (bc.baseTopEdge[0] / 2) + bc.baseTopLeft[0]
+            bc.baseMidY = (bc.baseRightEdge[1] / 2) + bc.baseTopRight[1]
+            bc.baseMidPoint = (bc.baseMidX, bc.baseMidY)
+            bc.maxTagDisplacement = bc.baseMidPoint[0] - (bc.baseTopEdge[0] / 2)
 
+            return bc
 
+    return None
 
 
 def destroy():
     picar_helper.stop()
     camera.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     try:
--- src/RobotServer/picar_server.py
+++ src/RobotServer/picar_server.py
@@ -9,86 +9,69 @@ import cv2
 import picar_pb2
 import picar_pb2_grpc
 
-import os
-
-#Variables to control this picar's behavior
-mode = 0
-throttle = 0
-direction = 0
-image = None
 
 class PiCarServicer(picar_pb2_grpc.PiCarServicer):
-	"""Provides methods that implement functionality of PiCar server."""
-	
-	def __init__(self):
-		global image
-		image = cv2.imread('init.jpg')
+    """Provides methods that implement functionality of PiCar server."""
 
-	def ReceiveConnection(self, request, context):
-		"""Handshake between PiCar and desktop application"""
-		print('Received connection request from %s' % request.message)
-		#Send a ConnectAck message showing success
-		return picar_pb2.ConnectAck(success=True)
+    def __init__(self, driver):
+        self.driver = driver
+        self.streaming = False
 
-	def SwitchMode(self, request, context):
-		"""Changes the operating mode of the PiCar"""
-		global mode
-		if (mode != request.mode):
-			#If the request is for a different mode, send a success ack
-			print('Switching mode from %s to %s' % (mode, request.mode))
-			mode = request.mode
-			return picar_pb2.ModeAck(success=True)
-		else:
-			#If the request is for the mode already in, send a failure ack
-			print('Request received for mode %s, but already in that mode!' % (request.mode))
-			return picar_pb2.ModeAck(success=False)
+    def ReceiveConnection(self, request, context):
+        """Handshake between PiCar and desktop application"""
+        print('Received connection request from %s' % request.message)
+        # Send a ConnectAck message showing success
+        return picar_pb2.ConnectAck(success=True)
 
-	def RemoteControl(self, request, context):
-		"""Receive control data from desktop application"""
-		#Clamp the input throttle and direction to [-1, 1]
-		global throttle
-		global direction
-		throttle = max(-1, min(request.throttle, 1))
-		direction = max(-1, min(request.direction, 1))
-		print('Setting wheels to %f throttle and %f steering' % (throttle, direction))
-		return picar_pb2.Empty()
-		
-	def VideoStream(self, request, context):
-		"""Send back images captured from webcam, encoded as jpeg"""
-		global image
-		self.streaming = True
-		print('Starting video stream')
-		
-		while(self.streaming):
-			image = cv2.resize(image, (320, 240))
-			encoded, buffer = cv2.imencode('.jpg', image)
-			bytes = buffer.tobytes()
-			
-			message = picar_pb2.ImageCapture(image=bytes) #Create message with image
-			#print('Sending frame')
-			yield message #Send it				
-			sleep(1 / 24) #24Hz refresh rate
-	
-	def StopStream(self, request, context):
-		"""Stop the sending of a video stream"""
-		self.streaming = False
-		print('Stopping video stream')
-		return picar_pb2.Empty()
-		
-def getServer():
-	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-	picar_pb2_grpc.add_PiCarServicer_to_server(
-		PiCarServicer(), server)
-	server.add_insecure_port('[::]:50051')
-	return server
-	
-def setFrame(frame):
-	"""Set the image that is sent over network from a captured webcam frame"""
-	global image
-	image = frame
-		
-if __name__ == '__main__':
-	server = getServer()
-	server.start()
-	print('Server started')
-	sleep(60*60*24)
+    def SwitchMode(self, request, context):
+        """Changes the operating mode of the PiCar"""
+        if self.driver.mode != request.mode:
+            # If the request is for a different mode, send a success ack
+            print('Switching mode from %s to %s' % (
+                self.driver.mode, request.mode))
+            self.driver.mode = request.mode
+            return picar_pb2.ModeAck(success=True)
+        else:
+            # If the request is for the mode already in, send a failure ack
+            print(
+                    'Request received for mode %s, but already in that mode!' % request.mode)
+            return picar_pb2.ModeAck(success=False)
+
+    def RemoteControl(self, request, context):
+        """Receive control data from desktop application"""
+        # Clamp the input throttle and direction to [-1, 1]
+        throttle = max(-1, min(request.throttle, 1))
+        direction = max(-1, min(request.direction, 1))
+        print('Setting wheels to %f throttle and %f steering' % (
+            throttle, direction))
+        self.driver.set_throttle_and_dir(throttle, direction)
+        return picar_pb2.Empty()
+
+    def VideoStream(self, request, context):
+        """Send back images captured from webcam, encoded as jpeg"""
+        self.streaming = True
+        print('Starting video stream')
+
+        while self.streaming:
+            image = cv2.resize(self.driver.frame, (320, 240))
+            _, b = cv2.imencode('.jpg', image)
+            b = b.tobytes()
+
+            message = picar_pb2.ImageCapture(
+                image=b)  # Create message with image
+            yield message  # Send it
+            sleep(1 / 24)  # 24Hz refresh rate
+
+    def StopStream(self, request, context):
+        """Stop the sending of a video stream"""
+        self.streaming = False
+        print('Stopping video stream')
+        return picar_pb2.Empty()
+
+
+def getServer(driver):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    picar_pb2_grpc.add_PiCarServicer_to_server(
+        PiCarServicer(driver), server)
+    server.add_insecure_port('[::]:50051')
+    return server
