88bf521fd2ce0f06da6564d9e16c86f31d392f86
ehaskell1 <ehaskell@oakland.edu>
2019-02-09 18:41:57 -0500

    Fix zombie PiCar (#23)
    Change remote control to a stream and fix zombie PiCar issue
    
    Closes #13

--- src/RobotServer/picar_helper.py
+++ src/RobotServer/picar_helper.py
@@ -8,43 +8,48 @@ bw = back_wheels.Back_Wheels()
 fw = front_wheels.Front_Wheels()
 hostname = socket.gethostname()
 
+
 def move(throttle, direction):
     fw_default = getDefaultAngle()
-    FW_ANGLE_MAX = fw_default+30
-    FW_ANGLE_MIN = fw_default-30
+    fw_angle_max = fw_default + 30
+    fw_angle_min = fw_default - 30
     rear_wheels_enabled = True
     front_wheels_enabled = True
 
-    motor_speed = int(abs(throttle)*100)
-    fw_angle = fw_default+(30*(direction))
+    motor_speed = int(abs(throttle) * 100)
+    fw_angle = fw_default + (30 * direction)
 
-    if front_wheels_enabled and (fw_angle >= FW_ANGLE_MIN and fw_angle <= FW_ANGLE_MAX):
+    if front_wheels_enabled and (fw_angle_min <= fw_angle <= fw_angle_max):
         fw.turn(fw_angle)
     if rear_wheels_enabled:
-        if (throttle > 0.0):
+        if throttle > 0.0:
             move_forward(motor_speed)
-        elif (throttle < 0.0):
+        elif throttle < 0.0:
             move_backward(motor_speed)
         else:
             stop()
-			
+
 
 def move_forward(speed):
     bw.speed = speed
     bw.backward()
 
+
 def move_backward(speed):
     bw.speed = speed
     bw.forward()
 
+
 def stop():
     bw.stop()
+
 
 def getDefaultAngle():
     if hostname == 'picarA':
         return picar_a_angle
     elif hostname == 'picarB':
         return picar_b_angle
+
 
 """
 Wheel angle above 90 is to the right, below 90 is to the left
