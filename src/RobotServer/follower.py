a16b00540bf576f1570bcb7712260e200797aef4
Justin Kur <jakur@oakland.edu>
2019-02-11 04:27:36 -0500

    Connect backend to trained network

---
+++ src/RobotServer/follower.py
@@ -0,0 +1,24 @@
+import fastai
+import torch
+import PIL
+from fastai.vision import *
+import cv2
+
+class Follower:
+    def __init__(self):
+        self.model = load_learner("models", fname="supervised.pkl")
+
+    def get_action(self, frame):
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = PIL.Image.fromarray(img)
+        img = pil2tensor(img, np.float32)
+        img.div_(255)
+        result = self.model.predict(Image(img))
+        print(result[0])
+
+
+if __name__ == '__main__':
+    follower = Follower()
+    image = cv2.imread("C:/Code/Senior Project/train/mirrorB_2605.jpg")
+
+    follower.get_action(image)
