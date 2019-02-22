ad76fdd10fb61843b2d3aa6d3aa4f7209c55519e
Josh B <jjbaird@oakland.edu>
2019-02-22 17:51:14 -0500

    Update tag thresholds when classifing

--- src/RobotServer/RL/classify_training_data.py
+++ src/RobotServer/RL/classify_training_data.py
@@ -13,6 +13,9 @@ IMAGE_DIR = "train"
 IN_CSV = "labels.csv"  # For the action done
 OUT_CSV = "rl_Labels.csv"
 
+# Training data is 320x240 but pi camera is 640x480
+tag_detector.NEAR_THRESHOLD /= 2
+tag_detector.FAR_THRESHOLD /= 2
 
 def main():
     os.chdir(BASE_PATH)
@@ -24,17 +27,20 @@ def main():
         img = Image.open("{0}/{1}".format(IMAGE_DIR, file_name))
         img = np.array(img)
 
-        # loc = tag_detector.tag_loc(img)
-        # state = tag_detector.state_from_loc(loc)
+        loc = tag_detector.tag_loc(img)
+        state = tag_detector.state_from_loc(loc)
 
-        # print(loc.x_pos, loc.avg_edge_len)
+        if loc is not None:
+            print(loc.x_pos, loc.avg_edge_len)
+            print(state)
 
-        state = tag_detector.state_from_frame(img)
+        # state = tag_detector.state_from_frame(img)
 
         classifications = classifications.append(
             {"file_name": file_name, "state": state}, ignore_index=True)
 
     print(classifications)
+    print(classifications['state'].value_counts())
     classifications.to_csv(OUT_CSV, index=False)
 
 
