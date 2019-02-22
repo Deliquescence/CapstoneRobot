bfe8fa6f6b03c4da5fdad7a8c10b582a138da515
Josh B <jjbaird@oakland.edu>
2019-02-22 18:48:50 -0500

    Classify actions of training data
    todo: reward, next state

--- src/RobotServer/RL/classify_training_data.py
+++ src/RobotServer/RL/classify_training_data.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 import tag_detector
 from states import State
+from actions import Action
 
 
 BASE_PATH = "../../../../train_data"
@@ -17,6 +18,48 @@ OUT_CSV = "rl_Labels.csv"
 tag_detector.NEAR_THRESHOLD /= 2
 tag_detector.FAR_THRESHOLD /= 2
 
+
+STOP_THRESHOLD = 0.01
+LOW_THRESHOLD = 0.4
+
+STRAIGHT_THRESHOLD = 0.1
+SOFT_THRESHOLD = 0.5
+
+
+def map_action(throttle, direction):
+    """Return the action representing the given throttle and direction."""
+
+    if throttle < -1*LOW_THRESHOLD:
+        actions = [Action.rev_high_hard_left, Action.rev_high_soft_left,
+                   Action.rev_high_straight, Action.rev_high_soft_right, Action.rev_high_hard_right]
+
+    elif throttle < -1*STOP_THRESHOLD:
+        actions = [Action.rev_low_hard_left, Action.rev_low_soft_left,
+                   Action.rev_low_straight, Action.rev_low_soft_right, Action.rev_low_hard_right]
+
+    elif throttle <= STOP_THRESHOLD:
+        actions = [Action.stop]*5
+
+    elif throttle <= LOW_THRESHOLD:
+        actions = [Action.low_hard_left, Action.low_soft_left,
+                   Action.low_straight, Action.low_soft_right, Action.low_hard_right]
+
+    else:
+        actions = [Action.high_hard_left, Action.high_soft_left,
+                   Action.high_straight, Action.high_soft_right, Action.high_hard_right]
+
+    if direction < -1*SOFT_THRESHOLD:
+        return actions[0]
+    elif direction < -1*STRAIGHT_THRESHOLD:
+        return actions[1]
+    elif direction <= STRAIGHT_THRESHOLD:
+        return actions[2]
+    elif direction <= SOFT_THRESHOLD:
+        return actions[3]
+    else:
+        return actions[4]
+
+
 def main():
     os.chdir(BASE_PATH)
 
@@ -24,7 +67,8 @@ def main():
 
     for file_name in os.listdir(IMAGE_DIR):
         print(file_name)
-        img = Image.open("{0}/{1}".format(IMAGE_DIR, file_name))
+        file_name_with_folder = "{0}/{1}".format(IMAGE_DIR, file_name)
+        img = Image.open(file_name_with_folder)
         img = np.array(img)
 
         loc = tag_detector.tag_loc(img)
@@ -37,11 +81,21 @@ def main():
         # state = tag_detector.state_from_frame(img)
 
         classifications = classifications.append(
-            {"file_name": file_name, "state": state}, ignore_index=True)
+            {"file_name": file_name_with_folder, "state": state}, ignore_index=True)
 
-    print(classifications)
+        # break
+
     print(classifications['state'].value_counts())
     classifications.to_csv(OUT_CSV, index=False)
+
+    df = classifications.merge(
+        pd.read_csv(IN_CSV), left_on="file_name", right_on="image_file", how="inner")
+
+    df['action'] = df.apply(lambda row: map_action(
+        row['throttle'], row['direction']), axis=1)
+
+    rl_labels = df[['state', 'action']]
+    print(rl_labels)
 
 
 if __name__ == '__main__':
