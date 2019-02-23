ce814b04f87116bdaf44696c9194c9c1fc67256e
Josh B <jjbaird@oakland.edu>
2019-02-22 19:16:47 -0500

    Determine reward when classifing training data

--- src/RobotServer/RL/classify_training_data.py
+++ src/RobotServer/RL/classify_training_data.py
@@ -60,10 +60,24 @@ def map_action(throttle, direction):
         return actions[4]
 
 
+def reward(tag_loc, action):
+    if tag_loc is None:
+        return 0.0
+
+    edge_len = tag_loc.avg_edge_len
+    # todo? squared distance error. Would need fn for tag_length -> distance.
+    if edge_len < tag_detector.NEAR_THRESHOLD and edge_len > tag_detector.FAR_THRESHOLD:
+        return 1.0
+    else:
+        return -1.0
+
+    return 0.0
+
+
 def main():
     os.chdir(BASE_PATH)
 
-    classifications = pd.DataFrame(columns=['file_name', 'state'])
+    df = pd.DataFrame(columns=['file_name', 'state', 'tag_loc'])
 
     for file_name in os.listdir(IMAGE_DIR):
         file_name_with_folder = "{0}/{1}".format(IMAGE_DIR, file_name)
@@ -82,18 +96,21 @@ def main():
 
         # state = tag_detector.state_from_frame(img)
 
-        classifications = classifications.append(
-            {"file_name": file_name_with_folder, "state": state}, ignore_index=True)
+        df = df.append(
+            {"file_name": file_name_with_folder, "state": state, "tag_loc": loc}, ignore_index=True)
 
-    print(classifications['state'].value_counts())
+    print(df['state'].value_counts())
 
-    df = classifications.merge(
+    df = df.merge(
         pd.read_csv(IN_CSV), left_on="file_name", right_on="image_file", how="inner")
 
     df['action'] = df.apply(lambda row: map_action(
         row['throttle'], row['direction']), axis=1)
 
-    rl_labels = df[['state', 'action']]
+    df['reward'] = df.apply(lambda row: reward(
+        row['tag_loc'], row['action']), axis=1)
+
+    rl_labels = df[['state', 'action', 'reward']]
     print(rl_labels)
     rl_labels.to_csv(OUT_CSV, index=False)
 
