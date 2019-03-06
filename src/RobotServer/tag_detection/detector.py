273f097b8bd32cbb385d8d6943dd5a63f434d0fe
Josh Baird <jjbaird@oakland.edu>
2019-03-06 00:32:15 -0500

    Pose estimation

--- src/RobotServer/tag_detection/detector.py
+++ src/RobotServer/tag_detection/detector.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from .calibrate import get_calibration
 
 AR_DICT = cv2.aruco.Dictionary_create(1, 3)
 AR_PARAMS = cv2.aruco.DetectorParameters_create()
@@ -47,3 +48,49 @@ def tag_loc(frame):
         return PreciseLocation(avg_edge / width, tag_mid_x)
     else:
         return None
+
+
+def estimate_pose(frame):
+    TAG_SIZE = 3 # inches
+
+    cam_mtx, dist = get_calibration()
+
+    # setting up our frame
+    img = np.array(frame, dtype=np.uint8)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # gather the parameters of the markers
+    corners, ids, _reject = cv2.aruco.detectMarkers(gray, AR_DICT, parameters=AR_PARAMS, cameraMatrix=cam_mtx, distCoeff=dist)
+
+    if ids is None:
+        return None
+
+    rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, TAG_SIZE, cam_mtx, dist)
+
+    # http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/OWENS/LECT9/img4.gif
+
+    # Rotations:
+    # [x, z, y] in rvecs
+    # x: unclear but we don't really care about it
+    # y: (I think) "lead car is turned right" -> negative, "lead car is turned left" -> positive
+    # z: counterclockwise -, clockwise +
+
+    # Translations:
+    # [x, y, z] in tvecs
+    # x: right +, left -
+    # y: top -, bottom +
+    # z: close -, far +
+
+    rx = rvecs[0][0][0]
+    rz = rvecs[0][0][1]
+    ry = rvecs[0][0][2]
+
+    tx = tvecs[0][0][0]
+    ty = tvecs[0][0][1]
+    tz = tvecs[0][0][2]
+
+    # print(rvecs)
+    # print(tvecs)
+    # print(_objPoints)
+
+    return rvecs, tvecs
---
+++ src/RobotServer/test_tag_detection.py
@@ -0,0 +1,52 @@
+import time
+import cv2
+from tag_detection.detector import estimate_pose
+
+
+def main():
+    camera = cv2.VideoCapture(0)
+
+    try:
+        while True:
+            start_time = time.time()
+            # https://stackoverflow.com/a/48297397
+            for _ in range(5):
+                camera.grab()
+            _, frame = camera.read()
+            frame_time = time.time()
+
+            ret = estimate_pose(frame)
+
+            detect_time = time.time()
+            print('Got frame in {0} sec and detection ran in {1} sec'
+                  .format(frame_time - start_time, detect_time - frame_time))
+
+            if ret is not None:
+                rvecs = ret[0]
+                tvecs = ret[1]
+
+                print(rvecs)
+                print(tvecs)
+
+            else:
+                print('Could not detect marker')
+
+            #time.sleep(1)
+            s = input('Press q <Enter> to quit or c <Enter> to continue: ')
+            if s == 'q':
+                break
+
+    finally:
+        camera.release()
+
+
+def from_file():
+
+    fname = "C:/picar/train/tag_orientation_00440.jpg"
+    image = cv2.imread(fname)
+    print(fname)
+    ret = normal_vector(image)
+
+if __name__ == '__main__':
+    #from_file()
+    main()
