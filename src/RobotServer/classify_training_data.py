b1409bb60ef60dae8aba57651a83085ad583f9e5
Josh Baird <jjbaird@oakland.edu>
2019-03-17 17:29:24 -0400

    Update classify_training_data.py (pose est, states, actions)

--- src/RobotServer/classify_training_data.py
+++ src/RobotServer/classify_training_data.py
@@ -5,9 +5,9 @@ import os
 from PIL import Image
 
 from tag_detection.detector import estimate_pose
-import follower
-import RL.states
-import RL.actions
+from follower import Follower
+from RL.states import state_from_pose
+from RL.actions import action_from_throttle_direction
 
 BASE_PATH = "../../../train_data"
 IMAGE_DIR = "train"
@@ -15,25 +15,11 @@ IN_CSV = "labels.csv"  # For the action done
 OUT_CSV = "rl_Labels.csv"
 
 
-def reward(tag_loc, action):
-    if tag_loc is None:
-        return 0.0
-
-    edge_len = tag_loc.avg_edge_len
-    # todo? squared distance error. Would need fn for tag_length -> distance.
-    if edge_len < NEAR_THRESHOLD and edge_len > FAR_THRESHOLD:
-        return 1.0
-    else:
-        return 0.0
-
-    return 0.0
-
-
 def offset_state(df, row, offset):
     try:
         offset_row = df.iloc[row.name + offset]
     except IndexError:
-        return State.unknown
+        return 0
 
     current_name_splits = row['file_name'].split('_')
     offset_name_splits = offset_row['file_name'].split('_')
@@ -41,14 +27,14 @@ def offset_state(df, row, offset):
         # Not same training label
         # print("{} and {} are not same label".format(
         #     offset_name_splits[0], current_name_splits[0]))
-        return State.unknown
+        return 0
 
     current_num = current_name_splits[1].split(".")[0]
     offset_num = offset_name_splits[1].split(".")[0]
     if int(offset_num) != int(current_num) + offset:
         # Not consecutive
         # print("{} and {} are not consecutive".format(offset_num, current_num))
-        return State.unknown
+        return 0
 
     return offset_row['state']
 
@@ -57,9 +43,9 @@ def unknown_state_cache(previous_state, state):
     """If the current state is not unknown, pass it through.
     If the current state is unknown but the previous state is known, use that.
     If both are unknown, then unknown."""
-    if state != State.unknown:
+    if state != 0:
         return state
-    elif previous_state != State.unknown:
+    elif previous_state != 0:
         return previous_state
     else:
         return state
@@ -68,25 +54,32 @@ def unknown_state_cache(previous_state, state):
 def main():
     os.chdir(BASE_PATH)
 
-    df = pd.DataFrame(columns=['file_name', 'state', 'tag_loc'])
+    df = pd.DataFrame(columns=['file_name', 'state', 'tz', 'tx'])
 
     for file_name in os.listdir(IMAGE_DIR):
         file_name_with_folder = "{0}/{1}".format(IMAGE_DIR, file_name)
         img = Image.open(file_name_with_folder)
         img = np.array(img)
 
-        loc = tag_loc(img)
-        state = state_from_loc(loc)
+        # TODO save and load the pose for each image so we don't have to recalculate every time
+        pose = estimate_pose(img)
+        state = state_from_pose(pose)
+
+        if pose is None:
+            tz = None
+            tx = None
+        else:
+            _rotation, translation, _, _ = pose
+            [tx, _ty, tz] = translation
 
         logging = False
         if logging:
             print(file_name)
-            if loc is not None:
-                print(loc.x_pos, loc.avg_edge_len)
-                print(state)
+            print(tz, tx)
+            print(state)
 
         df = df.append(
-            {"file_name": file_name_with_folder, "state": state, "tag_loc": loc}, ignore_index=True)
+            {'file_name': file_name_with_folder, 'state': state, 'tz': tz, 'tx': tx}, ignore_index=True)
 
     print(df['state'].value_counts())
     print(df)
@@ -94,18 +87,28 @@ def main():
     df = df.merge(
         pd.read_csv(IN_CSV), left_on="file_name", right_on="image_file", how="inner", validate="1:1")
 
-    df['action'] = df.apply(lambda row: actions.from_throttle_direction(
+    df['action'] = df.apply(lambda row: action_from_throttle_direction(
         row['throttle'], row['direction']), axis=1)
 
-    df['reward'] = df.apply(lambda row: reward(
-        row['tag_loc'], row['action']), axis=1)
+    try:
+        follower = Follower.load()
+    except IOError:  # file does not exist
+        follower = Follower()
+
+    # TODO handle episodes
+    # TODO save and load the pose for each image so we don't have to recalculate every time
+    def row_to_reward(row):
+        frame = cv2.imread(row['file_name'])
+        features = follower.get_features(frame)
+        return follower.get_reward(features)
+
+    df['reward'] = df.apply(row_to_reward, axis=1)
 
     # One state unknown buffer
     df['state'] = df.apply(lambda row: unknown_state_cache(
         offset_state(df, row, -1), row['state']), axis=1)
 
     # Save tag classifications for manual inspection
-    # TODO save the exact detection data so it can be loaded instead of always rerunning
     df[['file_name', 'state', 'throttle', 'direction', 'action']].to_csv(
         "categorized.csv", index=False)
 
